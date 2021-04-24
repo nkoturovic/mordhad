4fcb72df84bd093cc4d8bfc9dc47ba7928c8c1eb
Nebojša Koturović <necer95@gmail.com>
2021-04-24 14:40:09 +0200

    refactor(lib): Reorganize internal structure of serbian alphabet module
    Change Either and Char types to algebraic data type for Cyr and Latin
    letters respectively, providing stronger and richer types.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,46 +1,61 @@
 module Lib
-    ( srCyrAlphabet
-    , srLatinAlphabet
-    , cyrToLatin
-    , fromCyr
+    ( cyrToLatin
     ) where
 
 import Data.List
 import Control.Monad
 
-srCyrAlphabet :: [Char]
-srCyrAlphabet = 
-    ['а', 'б', 'в', 'г', 'д', 'ђ', 'е', 'ж', 'з', 'и',
-     'ј', 'к', 'л', 'љ', 'м', 'н', 'њ', 'о', 'п', 'р',
-     'с', 'т', 'ћ', 'у', 'ф', 'х', 'ц', 'ч', 'џ', 'ш',
-     'А', 'Б', 'В', 'Г', 'Д', 'Ђ', 'Е', 'Ж', 'З', 'И',
-     'Ј', 'К', 'Л', 'Љ', 'М', 'Н', 'Њ', 'О', 'П', 'Р', 
-     'С', 'Т', 'Ћ', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Џ', 'Ш']
+data LatinLetter = One Char 
+                 | Two (Char, Char)
+    deriving (Eq)
 
-srLatinAlphabet :: [Either Char (Char, Char)]
-srLatinAlphabet = 
-    [ Left 'a', Left 'b', Left 'v', Left 'g', Left 'd',
-      Left 'đ', Left 'e', Left 'ž', Left 'z', Left 'i', 
-      Left 'j', Left 'k', Left 'l', Right ('l', 'j'), Left 'm', 
-      Left 'n', Right ('n', 'j'), Left 'o', Left 'p', Left 'r', 
-      Left 's', Left 't', Left 'ć', Left 'u', Left 'f',
-      Left 'h', Left 'c', Left 'č', Right ('d', 'ž'), Left 'š',
-      Left 'A', Left 'B', Left 'V', Left 'G', Left 'D',
-      Left 'Đ', Left 'E', Left 'Ž', Left 'Z', Left 'I',
-      Left 'J', Left 'K', Left 'L', Right ('L', 'J'), Left 'M',
-      Left 'N', Right ('N', 'J'), Left 'O', Left 'P', Left 'R',
-      Left 'S', Left 'T', Left 'Ć', Left 'U', Left 'F',
-      Left 'H', Left 'C', Left 'Č', Right ('D', 'ž'), Left 'Š'
-    ]
+latinLetterStr :: LatinLetter -> String
+latinLetterStr (One c) = [c]
+latinLetterStr (Two (c1, c2)) = [c1, c2]
 
-fromCyr :: Char -> Either Char (Char, Char)
-fromCyr c = case c `elemIndex` srCyrAlphabet of
-              Nothing -> Left c
-              Just idx -> srLatinAlphabet !! idx
+newtype CyrLetter = Cyr Char
+    deriving (Eq)
 
-toString :: Either Char (Char, Char) -> String
-toString (Left c) = [c]
-toString (Right (c1, c2)) = [c1, c2]
+cyrFromChar :: Char -> Maybe CyrLetter
+cyrFromChar c = find (== Cyr c) cyrAlphabet
 
-cyrToLatin :: String -> String
-cyrToLatin = concatMap (toString . fromCyr)
+cyrAlphabet :: [CyrLetter]
+cyrAlphabet = 
+    [Cyr 'а', Cyr 'б', Cyr 'в', Cyr 'г', Cyr 'д',
+     Cyr 'ђ', Cyr 'е', Cyr 'ж', Cyr 'з', Cyr 'и',
+     Cyr 'ј', Cyr 'к', Cyr 'л', Cyr 'љ', Cyr 'м', 
+     Cyr 'н', Cyr 'њ', Cyr 'о', Cyr 'п', Cyr 'р',
+     Cyr 'с', Cyr 'т', Cyr 'ћ', Cyr 'у', Cyr 'ф',
+     Cyr 'х', Cyr 'ц', Cyr 'ч', Cyr 'џ', Cyr 'ш',
+     Cyr 'А', Cyr 'Б', Cyr 'В', Cyr 'Г', Cyr 'Д',
+     Cyr 'Ђ', Cyr 'Е', Cyr 'Ж', Cyr 'З', Cyr 'И',
+     Cyr 'Ј', Cyr 'К', Cyr 'Л', Cyr 'Љ', Cyr 'М', 
+     Cyr 'Н', Cyr 'Њ', Cyr 'О', Cyr 'П', Cyr 'Р', 
+     Cyr 'С', Cyr 'Т', Cyr 'Ћ', Cyr 'У', Cyr 'Ф', 
+     Cyr 'Х', Cyr 'Ц', Cyr 'Ч', Cyr 'Џ', Cyr 'Ш']
+
+latinAlphabet :: [LatinLetter]
+latinAlphabet = 
+    [ One 'a', One 'b', One 'v', One 'g', One 'd',
+      One 'đ', One 'e', One 'ž', One 'z', One 'i', 
+      One 'j', One 'k', One 'l', Two ('l', 'j'), One 'm', 
+      One 'n', Two ('n', 'j'), One 'o', One 'p', One 'r', 
+      One 's', One 't', One 'ć', One 'u', One 'f',
+      One 'h', One 'c', One 'č', Two ('d', 'ž'), One 'š',
+      One 'A', One 'B', One 'V', One 'G', One 'D',
+      One 'Đ', One 'E', One 'Ž', One 'Z', One 'I',
+      One 'J', One 'K', One 'L', Two ('L', 'J'), One 'M',
+      One 'N', Two ('N', 'J'), One 'O', One 'P', One 'R',
+      One 'S', One 'T', One 'Ć', One 'U', One 'F',
+      One 'H', One 'C', One 'Č', Two ('D', 'ž'), One 'Š']
+
+cyrToLatinLetter :: CyrLetter -> LatinLetter
+cyrToLatinLetter (Cyr c) = case Cyr c `elemIndex` cyrAlphabet of
+              Nothing -> One c
+              Just idx -> latinAlphabet !! idx
+
+cyrToLatin :: String -> String 
+cyrToLatin = concatMap $
+    \x -> case cyrFromChar x of
+        Nothing -> [x]
+        Just l -> latinLetterStr $ cyrToLatinLetter l
