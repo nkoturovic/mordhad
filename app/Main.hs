fbea5e1221dcffa4584d534001e4606e5201925f
Nebojša Koturović <necer95@gmail.com>
2021-04-26 16:34:26 +0200

    feat(SerbianAlphabet): Converting Latin to ASCII form
    Implement latinToAsciiForm func which enables transforming String
    containing Latin alphabet letters to their ACII form, for example:
     - Latin letter š -> ASCII letter s
     - Latin letter č -> ASCII letter c
     - Latin letter đ -> ASCII letter dj

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,4 @@ import qualified SerbianAlphabet as SRB
 
 main :: IO ()
 main = do 
-    putStrLn $ SRB.cyrToLatin "Здраво свете!!"
+    putStrLn $ SRB.latinToAsciiForm $ SRB.cyrToLatin "Здраво свете, студенти и ђаци!!"
